1cfe6b857401f496f9ee4b41550377af07eaf367
Kelsey <kpnewman2013@gmail.com>
2018-04-08 12:55:16 -0400

    starting from scratch with a different approach

--- api/inventory.js
+++ api/inventory.js
@@ -1,9 +1,18 @@
 require('isomorphic-fetch');
 
-fetch('http://autumn-resonance-1298.getsandbox.com/inventory')
+var inventory = fetch('http://autumn-resonance-1298.getsandbox.com/inventory')
     .then(function(response) {
         return response.json();
     })
-    .then(function(myJson){
-        console.log(myJson);
-    });
+    .then(function(inventory){
+        console.log(inventory);
+    }); 
+
+var products = fetch('http://autumn-resonance-1298.getsandbox.com/products')
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(products){
+        console.log(products);
+    });      
+    
